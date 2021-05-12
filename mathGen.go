a7b17660ca6873577c02671f29221ed5f37c0259
Jake Simmens <rocknjake@gmail.com>
2021-05-12 11:19:31 -0500

    Add subtraction problems function

--- mathGen.go
+++ mathGen.go
@@ -29,17 +29,6 @@ func main() {
 		quit = executeUserSelection(&problems)
 	}
 
-	//problemUsed := make([]bool, 100)
-
-	//additionProblems(3)
-
-	//fmt.Println(problems)
-	//display problem
-	//get user input
-	//check answer
-	//display correct or wrong
-	//show next problem
-
 	//track problems that are used and don't repeat
 }
 
@@ -65,6 +54,7 @@ func executeUserSelection(numSets *[]problemSet) bool {
 		additionProblems(3, *numSets)
 	case "2":
 		fmt.Println("Subtraction Time")
+		subtractionProblems(3, *numSets)
 	default:
 		fmt.Print("Invalid option.\n\n")
 		showUserMenu()
@@ -74,18 +64,38 @@ func executeUserSelection(numSets *[]problemSet) bool {
 }
 
 func additionProblems(count int, numSets []problemSet) {
-	//generate random addition problems between 2 numbers
-	// problems := make([]problemSet, 100)
-	// for i := 0; i < 10; i++ {
-	// 	for j := 0; j < 10; j++ {
-	// 		problems[i*10+j] = problemSet{i, j}
-	// 	}
-	// }
 
 	for i := 0; i < count; i++ {
 		problemNum := rand.Intn(100)
 		fmt.Printf(" %v + %v = ", numSets[problemNum].first, numSets[problemNum].second)
 		correctAnswer := numSets[problemNum].first + numSets[problemNum].second
+		var answer int
+		fmt.Scan(&answer)
+		if answer == correctAnswer {
+			fmt.Print("CORRECT\n\n")
+		} else {
+			fmt.Print("Answer is: ", correctAnswer, "\n\n")
+		}
+	}
+}
+
+func subtractionProblems(count int, numSets []problemSet) {
+
+	for i := 0; i < count; i++ {
+		var firstNum int
+		var secondNum int
+
+		problemNum := rand.Intn(100)
+		if numSets[problemNum].second > numSets[problemNum].first {
+			firstNum = numSets[problemNum].second
+			secondNum = numSets[problemNum].first
+		} else {
+			firstNum = numSets[problemNum].first
+			secondNum = numSets[problemNum].second
+		}
+
+		fmt.Printf(" %v - %v = ", firstNum, secondNum)
+		correctAnswer := firstNum - secondNum
 		var answer int
 		fmt.Scan(&answer)
 		if answer == correctAnswer {
